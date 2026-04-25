c32f1ab73941ddf478c78a0d91f4dc50d9053285
Rico <rekluz@gmail.com>
2026-04-24 22:20:23 -0700

    Delete app directory

--- app/src/main/java/com/rekluzgames/nikakudorimahjong/NikakudoriApplication.kt
+++
@@ -1,7 +0,0 @@
-package com.rekluzgames.nikakudorimahjong
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class NikakudoriApplication : Application()
--- app/src/main/java/com/rekluzgames/nikakudorimahjong/presentation/ui/component/StalemateOverlay.kt
+++
@@ -1,6 +0,0 @@
-/*
- * Copyright (c) 2026 Rekluz Games. All rights reserved.
- * StalemateOverlay has been moved to Overlays.kt
- */
-
-package com.rekluzgames.nikakudorimahjong.presentation.ui.component
